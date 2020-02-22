22451b92cd6881722e2407a9b18436c6cdeebe9c
Ouokki <ouokki98@gmail.com>
2020-02-23 00:07:49 +0100

    cpge-functions

--- cpge-functions/functions/index.js
+++ cpge-functions/functions/index.js
@@ -4,24 +4,15 @@ const {FBAuth}=require('./util/FBAuth');
 const {signUp}=require('./handlers/signup');
 const {login}=require('./handlers/login');
 const {admin,db}=require('./util/admin');
-
+const {firebaseConfig}=require('./util/config');
 const app=require('express')();
-const firebaseConfig = {
-  apiKey: "AIzaSyCURMxlNoDObmnMMOlsAAjY3pSrNxjEpB0",
-  authDomain: "cpge-app.firebaseapp.com",
-  databaseURL: "https://cpge-app.firebaseio.com",
-  projectId: "cpge-app",
-  storageBucket: "cpge-app.appspot.com",
-  messagingSenderId: "291077683094",
-  appId: "1:291077683094:web:d56b07afeb3775be444fb7",
-  measurementId: "G-RMNR7HP0GE"
-};
 const firebase=require('firebase');
+
+
 firebase.initializeApp(firebaseConfig);
 // Scream routes
 app.get('/screams',getALLscreams);
 app.post('/scream',FBAuth,postOneScream);
-// POST a scream
 // Login and registration routes 
 app.post('/SignUp',signUp);
 app.post('/login',login);
---
+++ cpge-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+exports.firebaseConfig = {
+    apiKey: "AIzaSyCURMxlNoDObmnMMOlsAAjY3pSrNxjEpB0",
+    authDomain: "cpge-app.firebaseapp.com",
+    databaseURL: "https://cpge-app.firebaseio.com",
+    projectId: "cpge-app",
+    storageBucket: "cpge-app.appspot.com",
+    messagingSenderId: "291077683094",
+    appId: "1:291077683094:web:d56b07afeb3775be444fb7",
+    measurementId: "G-RMNR7HP0GE"
+};
